c6be32838a6ef8a05381203e71bebe008bc03ac7
Vlad Shutchenko <vshutchenko@mail.ru>
2023-05-14 11:58:47 +0300

    Add project files.

---
+++ CombSort.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using CombSort.Lib;
+
+Console.WriteLine("Укажите путь к файлу:");
+var filePath = Console.ReadLine();
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("Файл не найден!");
+    return;
+}
+
+try
+{
+    var numbers = File.ReadAllLines(filePath).Select(int.Parse);
+
+    var sortedNumbers = numbers.CombSort();
+
+    Console.WriteLine("Введите путь для сохранения отсортированного файла:");
+    var savePath = Console.ReadLine();
+
+    File.WriteAllLines(savePath, sortedNumbers.Select(n => n.ToString()));
+
+    Console.WriteLine("Файл отсортирован и сохранен!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Возникла ошибка при работе с файлом: {ex.Message}");
+}
+
+Console.ReadLine();
---
+++ CombSort.Lib/CombSortExtension.cs
@@ -0,0 +1,46 @@
+﻿namespace CombSort.Lib;
+
+public static class CombSortExtension
+{
+    public static IEnumerable<T> CombSort<T>(this IEnumerable<T> values)
+        where T : IComparable<T>
+    {
+        return Sort(values.ToList());
+    }
+
+    private static IList<T> Sort<T>(IList<T> list)
+        where T : IComparable<T>
+    {
+        int n = list.Count;
+        int gap = n;
+        bool swapped = true;
+
+        while (gap > 1 || swapped)
+        {
+            gap = (int)(gap / 1.3);
+
+            if (gap < 1)
+            {
+                gap = 1;
+            }
+
+            int i = 0;
+            swapped = false;
+
+            while (i + gap < n)
+            {
+                if (list[i].CompareTo(list[i + gap]) > 0)
+                {
+                    var temp = list[i];
+                    list[i] = list[i + gap];
+                    list[i + gap] = temp;
+                    swapped = true;
+                }
+
+                i++;
+            }
+        }
+
+        return list;
+    }
+}
